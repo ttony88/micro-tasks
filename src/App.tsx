35c3977e0e8984c184b1f58e85d21e61656f869d
ttony62 <1988anton62@gmail.com>
2024-01-09 19:47:21 +0300

    add NewComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Header } from './site/header/Header';
-import { Body } from './site/body/Body';
-import { Footer } from './site/footer/Footer';
 
 function App() {
   return (
-    
+    <></>
   );
 }
 
